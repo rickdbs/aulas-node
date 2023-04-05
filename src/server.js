c6dc3e8b78b09fca73db4fa5bf5e789cafbf88dd
Henrique <rickdbs.dev@hotmail.com>
2023-04-05 13:34:44 -0300

    criação do banco de dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 require("express-async-errors")
-
+const database = require("./database/sqlite")
 
 const AppError = require("./utils/AppError")
 const express = require("express")
@@ -11,6 +11,10 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+
+database()
+
 
 app.use((error, request, response, next) => { 
     if(error instanceof AppError) {
